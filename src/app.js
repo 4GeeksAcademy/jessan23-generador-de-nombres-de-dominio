c538245dfb346cf5d693ccb00ea7e7632225e418
Jessan23 <jessicaanairayagrez@gmail.com>
2024-10-10 03:43:26 +0000

    entrega del generador de dominio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  // let generated = generatedDomains();
+  // generated.forEach(domain => console.log(domain));
+  //document.getElementById("generated").innerHTML = generatedDomains();
   console.log("Hello Rigo from the console!");
+
+  console.log("Este mensaje debería aparecer antes que el de window.onload");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensiones = [".com", ".net", ".us", ".io"];
+
+  function generatedDomains() {
+    //let domains = [];
+
+    for (let p of pronoun) {
+      for (let a of adj) {
+        for (let n of noun) {
+          for (let ext of extensiones) {
+            document.getElementById("generated").innerHTML += `<p>${p +
+              a +
+              n +
+              ext}</p> `;
+          }
+        }
+      }
+    }
+    //console.log(domains);
+
+    //return domains;
+  }
+  generatedDomains();
 };
